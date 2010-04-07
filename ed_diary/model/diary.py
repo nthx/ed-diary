29b3019240090fb26f92fbba57085e544ae645d1
Tomasz Nazar <tomasz.nazar@gmail.com>
2010-04-07 20:37:17 +0200

    Entry: polymorphic type when_for_ui method added; /home/user/.eddiar/diary.txt parsing added

--- ed_diary/model/diary.py
+++ ed_diary/model/diary.py
@@ -5,6 +5,15 @@ from ed_diary.model.entry import Entry
 import os
 
 
+HOME=os.environ.get('HOME') or '/home/user/'
+CONFIG_DIR=os.path.join(HOME, '.eddiary')
+DIARY_FILE=os.path.join(CONFIG_DIR, 'diary.txt')
+
+WELCOME="""
+Hello to "Every Day Diary". An utility for you to store a not each day.
+
+To continue just press Right Arrow.
+"""
 
 class Diary(object):
     """Represents a Diary. Diary has a list of entries.
@@ -15,8 +24,44 @@ class Diary(object):
 
     def import_me(self):
         self.entries = []
-        self.entries.append(Entry('abcdef'))
-        self.entries.append(Entry('123456\nXYZ'))
+
+        file, first_time = self.get_file_from_disk()
+        for entry in self.parse_file(file):
+            self.entries.append(entry)
+        file.close()
+
+        if not self.entries:
+            self.entries.append(Entry(WELCOME))
         return self
         
         
+
+    def get_file_from_disk(self):
+        first_time = False
+        if not os.path.exists(CONFIG_DIR):
+            os.mkdir(CONFIG_DIR)
+            first_time = True
+
+        if not os.path.exists(DIARY_FILE):
+            file = open(DIARY_FILE, 'w')
+            file.close()
+            first_time = True
+
+        return open(DIARY_FILE, 'r'), first_time
+        
+
+    def parse_file(self, file):
+        content = file.read()
+        lines = content.split('\n\n')
+
+        log.debug('%s entries' % len(lines))
+
+        for line in lines:
+            line = line.strip().strip('\n')
+            when, text = self.parse_entry(line)
+            yield Entry(text, when=when)
+
+
+    def parse_entry(self, line):
+        return '', line
+
--- ed_diary/model/entry.py
+++ ed_diary/model/entry.py
@@ -7,18 +7,41 @@ import os
 
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
+
+
 class Entry(object):
     """Represents one of Diary's entries
     """
-    def __init__(self, text):
+
+    POLYMORPHIC_WHEN_FOR_UI = {}
+    POLYMORPHIC_WHEN_FOR_UI[type('')] = lambda self: self.when_for_ui_for_str
+    POLYMORPHIC_WHEN_FOR_UI[type(datetime.datetime.utcnow())] = lambda self: self.when_for_ui_for_datetime
+
+
+    def __init__(self, text, when=datetime.datetime.utcnow()):
         self.text = text
-        self.when = datetime.datetime.utcnow()
+        self.when = when
+
+
+    def when_for_ui_for_str(self):
+        return str(self.when)
+
+
+    def when_for_ui_for_datetime(self):
+        return self.when.strftime(DATE_FORMAT)
 
 
     def when_for_ui(self):
-        return self.when and self.when.strftime(DATE_FORMAT) or ''
+        if not self.when:
+            self.when = ''
+
+        #Tricky Python polymorphism
+        fn = self.POLYMORPHIC_WHEN_FOR_UI[type(self.when)]
+        return fn(self)()
+
 
     def __str__(self):
         return '%s: %s' % (self.when, self.text)
 
     __repr__ = __str__
+
